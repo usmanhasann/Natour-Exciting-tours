3bf4eb5dcd694e6056c0dbced73fe45cebbaf28d
usmanhasann <osmanmuhammadofficial@gmail.com>
2024-12-12 16:01:30 +0500

    Clipper an image

--- src/Learning/Part1/Clip An Image.md
+++ src/Learning/Part1/Clip An Image.md
@@ -1,7 +1,13 @@
 # What is cliping an image ?
 
-https://www.w3schools.com/css/css3_gradients.asp
-🔝gradient
+-The clip-path CSS property creates a clipping region that sets what part of an element should be shown. Parts that are inside the region are shown, while those outside are hidden.
+-The polygon() CSS function is one of the <basic-shape> data types. It's used to draw a polygon by providing one or more pairs of coordinates, each of which represents a vertex of the shape.
+
+- when we want to show the picture that are cut to the edges
+  https://bennettfeely.com/clippy/
+  🔝make shape with clip property
+
+# What is transform property in css ?
 
 -
 -
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
+import Logo from "../assets/img/logo-white.png";
 const Header = () => {
   return (
     <>
       <header
-        className="h-[95vh] bg-hero bg-cover bg-top"
+        className="relative h-[95vh] bg-hero bg-cover bg-top"
         style={{
           background:
             "linear-gradient(to right bottom,rgba(126,213,111,0.8), rgba(40,180,131,0.8)),url('/src/assets/img/hero.jpg')",
         }}
       >
-        sssaa
+        <div className="logo-box absolute top-10 left-10 ">
+          <img src={Logo} alt="logo" className="h-[35px]" />
+        </div>
+        <h1 className="text-[#fff] uppercase">
+          <span className="">Outdoors</span>
+          <span>is where life happens</span>
+        </h1>
       </header>
     </>
   );
