c9ba79c9536a9ba2f92be957a1e2fab75fb3ad4b
usmanhasann <osmanmuhammadofficial@gmail.com>
2025-01-07 15:15:08 +0500

    learn box shadow

---
+++ src/Learning/Psedo Classes/learn.md
@@ -0,0 +1,11 @@
+Pseudo-classes CSS ka ek tareeka hain jo humein kisi element ki specific state ya position ko target karne mein madad karti hain, bina directly us element ko select kiye. Ye dynamic styling ke liye use hoti hain.
+
+Agar interview mein simple aur easy tareeke se samjhana ho, toh aap kuch is tarah bol sakte hain:
+
+"Pseudo-classes CSS ka ek feature hain jo humein kisi element ki khas state par styling lagane mein madad karta hai. Jaise agar koi user button ke upar hover kare, toh `:hover` pseudo-class us button ka style change kar sakti hai. Isi tarah, `:focus` kisi input field pe lagta hai jab user us field ko click karta hai. Ye cheezein website ko zyada interactive aur user-friendly banati hain."
+
+Ye ek casual aur relatable explanation hogi jo interviewer ko bhi samajhne mein asani hogi.
+
+link :::
+
+https://chatgpt.com/share/677cc159-5e48-800d-9e6c-1919222d6b7f
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,15 +13,21 @@ const Header = () => {
         <div className="logo-box absolute top-10 left-10  ">
           <img src={Logo} alt="logo" className="h-[35px] " />
         </div>
-        <div className="absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+        <div className="absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-center">
           <h1 className="text-[#fff] uppercase ">
-            <span className="heading-primary-main block font-[400] tracking-[35px] text-[60px]">
+            <span className="heading-primary-main block font-[400] tracking-[35px] text-[60px] hover:shadow-[15px_15px_30px_rgba(0,0,0,0.3)] rounded-[10rem] hover:translate-y-[-5px]">
               Outdoors
             </span>
-            <span className="heading-primary-main block text-[20px] tracking-[17.4px] font-[700]">
+            <span className="heading-primary-main block text-[20px] tracking-[17.4px] font-[700] hover:">
               is where life happens
             </span>
           </h1>
+          <a
+            className="bg-white text-center text-[rgb(119,119,119)] py-[15px] px-[40px] inline-block uppercase cursor-pointer rounded-[10rem] active:translate-y-[-1px] active:shadow-[0_5px_10px_rgba(0,0,0,0.2)] hover:translate-y-[-3px] hover:shadow-[0_10px_20px_rgba(0,0,0,0.2)]"
+            href="#"
+          >
+            Discover our tours
+          </a>
         </div>
       </header>
     </>
