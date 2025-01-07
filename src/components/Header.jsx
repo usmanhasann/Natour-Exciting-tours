cecf02b37687a37158ca767eccfe19324a55ded6
usmanhasann <osmanmuhammadofficial@gmail.com>
2025-01-07 17:35:00 +0500

    btn move up & header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import "../styles/Header-btn.css";
 import "../styles/Header.css";
 import Logo from "../assets/img/logo-white.png";
 const Header = () => {
@@ -23,7 +24,7 @@ const Header = () => {
             </span>
           </h1>
           <a
-            className="bg-white text-center text-[rgb(119,119,119)] py-[15px] px-[40px] inline-block uppercase cursor-pointer rounded-[10rem] active:translate-y-[-1px] active:shadow-[0_5px_10px_rgba(0,0,0,0.2)] hover:translate-y-[-3px] hover:shadow-[0_10px_20px_rgba(0,0,0,0.2)]"
+            className="bg-white text-center text-[rgb(119,119,119)] py-[15px] btn btn-white btn-animated px-[40px] inline-block uppercase cursor-pointer rounded-[10rem] active:translate-y-[-1px] active:shadow-[0_5px_10px_rgba(0,0,0,0.2)] hover:translate-y-[-3px] hover:shadow-[0_10px_20px_rgba(0,0,0,0.2)]"
             href="#"
           >
             Discover our tours
